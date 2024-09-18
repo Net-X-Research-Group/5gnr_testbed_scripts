1bf49f4315f0ed74f6a34f921b665235e89fce8e
Robert Hayek <hayekr2@gmail.com>
2024-09-18 17:03:07 -0500

    Add args and csv out

--- speedtest_automation.py
+++ speedtest_automation.py
@@ -13,6 +13,7 @@ import socket
 import fcntl
 import struct
 import pandas as pd
+import argparse
 
 def get_5g_ipaddr() -> str:
     interface = 'wwan0'
@@ -39,19 +40,25 @@ def parse_data(result: dict):
     timestamp = result['timestamp']
     return {'Timestamp': timestamp, 'Ping': ping, 'Download': download, 'Upload': upload}
 
-def main():
+def main(trials: int, filename: str):
     ip_addr = get_5g_ipaddr()
     print('IP Address of wwan0 is:', ip_addr)
     
     df = pd.DataFrame(columns=['Timestamp', 'Ping', 'Download', 'Upload'])
     
-    trials = 10
     for i in range(trials):
         print('Running Trial ', i)
         raw_output = run_test(ip_addr)
         output = parse_data(raw_output)
         df.loc[len(df.index)] = output
-    df.to_csv('output.csv', index=False)
+    df.to_csv(f'{filename}.csv', index=False)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='A tool to automate Ookla Speedtests')
+
+    parser.add_argument('-t', '--trials', type=int, help='The number of trials to run')
+    parser.add_argument('-o', '--output', help='Output file name (default csv)')
+    
+    args = parser.parse_args()
+            
+    main(args.trials, args.output)
