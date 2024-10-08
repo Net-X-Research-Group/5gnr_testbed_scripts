e2e0ab388778db63e75213ea3a580eb935655fda
Robert Hayek <hayekr2@gmail.com>
2024-10-08 12:43:03 -0500

    Update generate ssh file

--- generate_ssh_keys.py
+++ generate_ssh_keys.py
@@ -2,11 +2,30 @@ import os
 import paramiko
 import subprocess
 
-def generate_ssh_key(key_path="~/.ssh.id_rsa", key_comment)
-
-    key_path = os.path.expanduser(key_path)
+def generate_ssh_key(key_comment)
+    key_path = os.path.expanduser('~/.ssh/github_ssh_key')
     private_key_file = key_path
-    
-    return private_key, public_key
+    public_key_file = f'{key_path}.pub'
+    if os.path.exists(private_key_file) or os.path.exists(public_key_file):
+        print(f"Key files already exist at {private_key_file} and {public_key_file}.")
+        return private_key_file, public_key_file
+    return 0
 
-def transfer_keys(device_parameters: dict, private_key, public_key)
+def transfer_keys(devices: dict):
+    key_path = os.path.expanduser('~/.ssh/id_ed25519')
+    private_key_path = key_path
+    public_key_path = f'{key_path}.pub'
+    
+    paramiko.load
+
+
+    for device in devices:
+        scp_command_public = [
+            "sshpass", "-p", '5GController',
+            'scp', public_key_file, f'{device[username]@device[ip_addr]:{~/.ssh/id25519.pub}}'
+        ]
+        scp_command_private = [
+            "sshpass", "-p", '5GController',
+            'scp', private_key_file, f'{device[username]@device[ip_addr]:{~/.ssh/id25519}}'
+        ]
+
