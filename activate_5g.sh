b356a154b09a98b8595b9ba9c4ed65f14727cd9e
Robert Hayek <hayekr@users.noreply.github.com>
2025-03-04 00:36:33 -0600

    Update activate_5g.sh

--- activate_5g.sh
+++ activate_5g.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 # Function to run the original script
 run_script() {
-    sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='low-power'
-    sleep 1
-    sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online'
-    sleep 1
-    sudo ip link set wwan0 down
-    sleep 1
-    echo 'Y' | sudo tee /sys/class/net/wwan0/qmi/raw_ip
-    sleep 2
-    sudo ip link set wwan0 up
-    sleep 1
     sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='oai',ip-type=4" --client-no-release-cid
-    sleep 2
-    sudo qmicli -p -d /dev/cdc-wdm0 --wds-get-packet-service-status
-    sudo qmicli -p -d /dev/cdc-wdm0 --wds-get-current-settings
-    sleep 2
+    sleep 1
     # Modified to run udhcpc with -n so it doesn't kill the parent on failure
     sudo udhcpc -n -q -f -i wwan0 -t 5 || true
 }
