1d9ab7b0dcb941b46fc7b3e48624e4006fd1572b
Robert Hayek <hayekr2@gmail.com>
2025-05-29 15:38:55 -0500

    update start and ignore

--- start_telit_modem_qmicli.sh
+++ start_telit_modem_qmicli.sh
@@ -1,16 +1,9 @@
-sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='low-power'
-sleep 1
-sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online'
-sleep 1
-sudo ip link set wwan0 down
-sleep 1
-echo 'Y' | sudo tee /sys/class/net/wwan0/qmi/raw_ip
+$#!/bin/bash
+DEVICE=/dev/cdc-wdm0
+INTERFACE=wwan0
+
+sudo qmi-network $(DEVICE) start
 sleep 2
-sudo ip link set wwan0 up
-sleep 1
-sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='oai',ip-type=4" --client-no-release-cid
-sleep 2
-sudo qmicli -p -d /dev/cdc-wdm0 --wds-get-packet-service-status
-sudo qmicli -p -d /dev/cdc-wdm0 --wds-get-current-settings
-sleep 2
-sudo udhcpc -q -f -x lease:86400 -i wwan0
+
+# Get IP lease
+sudo udhcpc -q -f -x lease:86400 -i $(INTERFACE)
