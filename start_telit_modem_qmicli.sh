025cad3e68de368e678605346fb2ddfe7ef351f7
Robert Hayek <hayekr2@gmail.com>
2025-05-29 15:41:07 -0500

    Dont use vars

--- start_telit_modem_qmicli.sh
+++ start_telit_modem_qmicli.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-DEVICE=/dev/cdc-wdm0
-INTERFACE=wwan0
 
-sudo qmi-network $(DEVICE) start
+sudo qmi-network /dev/cdc-wdm0 start
 sleep 2
 
 # Get IP lease
-sudo udhcpc -q -f -x lease:86400 -i $(INTERFACE)
+sudo udhcpc -q -f -x lease:86400 -i wwan0
